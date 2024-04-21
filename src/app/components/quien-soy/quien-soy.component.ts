beacf35c03ef2f82cdbdd3986eb612fa9f8bbf02
GuidoGabrielPascucci <g.g.pascucci@gmail.com>
2024-04-20 22:39:44 -0300

    Comienzo del proyecto y primer commit

---
+++ src/app/components/quien-soy/quien-soy.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quien-soy',
+  standalone: true,
+  imports: [],
+  templateUrl: './quien-soy.component.html',
+  styleUrl: './quien-soy.component.css'
+})
+export class QuienSoyComponent {
+
+
+
+
+    
+
+    /*
+
+        - SUBIR A HOSTING WEB (Heroku o Firebase)
+        - FAVICON
+        - Componente de Login.
+        - Componente de Home.
+        - Componente “Quién Soy”
+            ● Datos personales del alumno.
+            ● Imagen del alumno
+            ● Explicación del juego propio
+        -
+
+    */
+}
