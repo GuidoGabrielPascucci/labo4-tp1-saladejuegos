c776648d5e411e828bea395a00b51d37c8f04b3b
GuidoGabrielPascucci <g.g.pascucci@gmail.com>
2024-04-27 15:27:19 -0300

    agrego conexion firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+    providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({ "projectId": "sala-de-juegos-d6eba", "appId": "1:17392839037:web:70f84504390e587d5cc414", "storageBucket": "sala-de-juegos-d6eba.appspot.com", "apiKey": "AIzaSyAXK25eTJ_in9YjeHVSukZ0_OJB2tna21I", "authDomain": "sala-de-juegos-d6eba.firebaseapp.com", "messagingSenderId": "17392839037" }))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+    public loginsCollection: any[] = [] 
+
+    public isThereData = false
+
+
+    constructor(private firestore: Firestore) {}
+
+    mostrarLogins() {
+
+        if (!this.isThereData) {
+            this.getDataFromFirebase()
+        }
+        
+        this.isThereData = true
+
+    }
+
+    getDataFromFirebase() {
+        let col = collection(this.firestore, 'logins')
+        const observable = collectionData(col)
+        observable.subscribe((respuesta) => {
+            // actualizamos nuestro array
+            this.loginsCollection = respuesta
+        })
+    }
+
+    limpiar() {
+        this.isThereData = false
+    }
+
 
 }
